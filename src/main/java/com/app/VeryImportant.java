1a9b3559aa979dd72edb4ed0ed7534fa891b293f
Ezaldeen Shoqairy <ezaldeenshoqairy@gmail.com>
2023-05-21 09:25:52 +0300

    Revert "Testing, values are not inserted in the chart correctly, must fix it!"
    This reverts commit 148e9132b73e315da502c33ae3a0c660b1e4a4f4.

--- src/main/java/com/app/VeryImportant.java
+++ src/main/java/com/app/VeryImportant.java
@@ -2,10 +2,7 @@ package com.app;
 
 import com.app.data.CitiesService;
 import com.app.factory.ChartInfo;
-import com.app.factory.ChartType;
-import com.app.factory.ChartsFactory;
 import com.jk.core.util.JK;
-import org.primefaces.model.charts.ChartModel;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -17,23 +14,6 @@ public class VeryImportant {
 
         CitiesService citiesService = CitiesService.getInstance();
         List<List<ChartInfo>> chartsInfos = new ArrayList<>();
-        List<ChartInfo> chartInfos = new ArrayList<>();
-        ChartInfo chartInfo = new ChartInfo(citiesService.getCitiesNames(),citiesService.getCitiesPopulations(),"","myChart", ChartType.BAR);
-        System.out.println(chartInfo.values());
-        chartInfos.add(chartInfo);
-        chartsInfos.add(chartInfos);
-        ChartsFactory chartsFactory = ChartsFactory.getInstance();
-        List<ChartModel> chartModels = chartsFactory.createCharts(chartsInfos);
-        for(ChartModel chartModel : chartModels){
-            System.out.println(chartModel.getData().getDataSet());
-        }
-
-        System.out.println("Done");
-
-
-
-
-
 
 
 
